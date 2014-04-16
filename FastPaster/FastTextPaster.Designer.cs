3d70e3b8139604d06d4aae0c379a1a370dd3c5cf
TDeMon <de_monForever@mail.ru>
2014-04-16 18:37:12 +0400

    third normal commit

---
+++ FastPaster/FastTextPaster.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace FastPaster
+{
+    partial class FastTextPaster
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextForPasting = new System.Windows.Forms.RichTextBox();
+            this.lstboxFiles = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.lblState = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextForPasting
+            // 
+            this.richTextForPasting.AcceptsTab = true;
+            this.richTextForPasting.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextForPasting.Location = new System.Drawing.Point(12, 26);
+            this.richTextForPasting.Name = "richTextForPasting";
+            this.richTextForPasting.Size = new System.Drawing.Size(920, 173);
+            this.richTextForPasting.TabIndex = 1;
+            this.richTextForPasting.Text = "";
+            // 
+            // lstboxFiles
+            // 
+            this.lstboxFiles.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstboxFiles.FormattingEnabled = true;
+            this.lstboxFiles.Location = new System.Drawing.Point(12, 231);
+            this.lstboxFiles.Name = "lstboxFiles";
+            this.lstboxFiles.Size = new System.Drawing.Size(843, 134);
+            this.lstboxFiles.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Text for insertion";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(781, 371);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 35);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Paste text";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(0, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(71, 34);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "+";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(0, 100);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(71, 34);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "-";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 215);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Files to insert";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Multiselect = true;
+            // 
+            // lblState
+            // 
+            this.lblState.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblState.AutoSize = true;
+            this.lblState.Location = new System.Drawing.Point(9, 392);
+            this.lblState.Name = "lblState";
+            this.lblState.Size = new System.Drawing.Size(35, 13);
+            this.lblState.TabIndex = 6;
+            this.lblState.Text = "State:";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(713, 208);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Filter for files in folders:";
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(0, 37);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(71, 57);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Add from directory";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::FastPaster.Properties.Settings.Default, "filtersSettings", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.textBox1.Location = new System.Drawing.Point(832, 205);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = global::FastPaster.Properties.Settings.Default.filtersSettings;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Location = new System.Drawing.Point(861, 231);
+            this.panel1.Margin = new System.Windows.Forms.Padding(1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(71, 134);
+            this.panel1.TabIndex = 9;
+            // 
+            // FastTextPaster
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(941, 414);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblState);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstboxFiles);
+            this.Controls.Add(this.richTextForPasting);
+            this.Name = "FastTextPaster";
+            this.Text = "Fast Text Paster";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextForPasting;
+        private System.Windows.Forms.ListBox lstboxFiles;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label lblState;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ FastPaster/FastTextPaster.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.IO;
+
+namespace FastPaster
+{
+
+    /// <summary>
+    /// This app places in to the beginig of every added file some text typed in richTextBox.
+    /// It was developed for fast adding some important comments to all source files at the same time.
+    /// </summary>
+    public partial class FastTextPaster : Form
+    {
+
+
+        public FastTextPaster()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Adding files to fileList
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog()==DialogResult.OK)
+            {
+                if (openFileDialog1.FileNames != null && openFileDialog1.FileNames.Length > 0)
+                {
+                    foreach (var item in openFileDialog1.FileNames)
+                    {
+                        lstboxFiles.Items.Add(item);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Removing the selected file from the fileList
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var removeItem = lstboxFiles.SelectedItem;
+            lstboxFiles.Items.Remove(removeItem);
+        }
+
+        /// <summary>
+        /// Initializes pasting process.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lblState.Text = "Pasting in process...";
+            foreach (var file in lstboxFiles.Items)
+            {
+                string fileContent = null;
+
+                using (FileStream fs = new FileStream((string)file, FileMode.Open))
+                using (StreamReader reader = new StreamReader(fs))
+                {
+                    fileContent = reader.ReadToEnd();                    
+                }                
+
+                using (FileStream fs = new FileStream((string)file, FileMode.Open))                
+                using (StreamWriter writer = new StreamWriter(fs))
+                {
+                    writer.WriteLine(richTextForPasting.Text);
+                    writer.Write(fileContent);
+                }
+
+            }
+
+            lblState.Text = "Text Pasted.";
+        }
+
+        /// <summary>
+        /// Adding files from all directories to the filelist.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string[] filters = textBox1.Text.Split(',');// Filters for files selection from directories
+
+            if (folderBrowserDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                List<string> dirs = new List<string>();
+                dirs.Add(folderBrowserDialog1.SelectedPath);
+                dirs.AddRange(RecursiveDirectoriesGetter(folderBrowserDialog1.SelectedPath));
+                foreach (var directory in dirs)
+                {
+                    var files = new List<string>();
+                    if (filters != null && filters.Length > 0)
+                    {
+                        foreach (var filter in filters)
+                        {
+                            files.AddRange(Directory.GetFiles(directory, filter));
+                        }
+                    }
+                    else
+                    {
+                        files.AddRange(Directory.GetFiles(directory));
+                    }
+                    foreach (var item in files)
+                    {
+                        lstboxFiles.Items.Add(item);
+                    }
+                }
+            }
+        }
+
+
+
+        string[] RecursiveDirectoriesGetter(string dir)
+        {
+            List<string> dirs = new List<string>();
+            dirs.AddRange(Directory.GetDirectories(dir));
+            List<string> rslt = new List<string>();
+            foreach(var item in dirs)
+            {
+                var tmp = RecursiveDirectoriesGetter(item);
+                if (tmp != null)
+                {
+                    rslt.AddRange(tmp);
+                }
+            }
+            dirs.AddRange(rslt.ToArray());
+            if (dirs != null)
+            {
+                return dirs.ToArray();
+            }
+            else
+            {
+                return null;
+            }
+        }
+       
+    }
+}
